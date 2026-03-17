c0d9886c3ce4bdaa8eabf2107dd35d8892cc26c8
bruiz611 <bruiz611@gmail.com>
2026-03-17 14:37:50 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+const link = document.getElementById("secretLink");
+
+link.addEventListener("click", function(e) {
+  e.preventDefault();
+  
+  alert("IP Logged. Notifying Craig...");
+
+  setTimeout(() => {
+    window.location.href = "https://www.max.com";
+  }, 1500);
+});
