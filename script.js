093d9807b469698ce941c65573bc74d2831801c8
bruiz611 <bruiz611@gmail.com>
2026-08-05 09:41:44 -0400

    Update script.js
    add craig's TDY counter JS

--- script.js
+++ script.js
@@ -9,3 +9,20 @@ link.addEventListener("click", function(e) {
     window.location.href = "https://www.max.com";
   }, 1500);
 });
+
+const startDate = new Date("2025-11-17T07:30:00");
+
+function updateCounter() {
+  const now = new Date;
+
+  const difference = now - startDate;
+
+  const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+
+  const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+}:
+
+document.getElementById("daysCounter").textContent = `${days} Days, ${hours} Hours`
+
+updateCounter();
+setInterval(UpdateCounter, 10000);
