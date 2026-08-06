c6f8c98c28ca06c094d76abc0ac859a388304c84
bruiz611 <bruiz611@gmail.com>
2026-08-05 20:55:32 -0400

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function updateCounter() {
   const difference = now - startDate;
   const days = Math.floor(difference / (1000 * 60 * 60 * 24));
   const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  const minutes = Math.floor(difference % (1000 * 60 * 60)) / (1000 * 60);
+  const minutes = Math.floor(difference % (1000 * 60 * 60)) / (1000 * 60));
   const seconds = Math.floor((difference % (1000 * 60)) / 1000);
 
   document.getElementById("daysCounter").textContent = `${days} Days, ${hours} Hours ${minutes} Minutes ${seconds} Seconds`;
